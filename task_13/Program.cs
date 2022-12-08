3f08e5df4f86af744552937fd860ca84be806c2f
Alexander <alex19952@gmail.com>
2022-12-08 22:26:29 +0300

    второе занятие

---
+++ task_13/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 13: Напишите программу, 
+// которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+string num;
+Console.Write("Введите целое число: ");
+num = Console.ReadLine();
+
+if (int.TryParse(num, out int temp) & num.Length > 3) {
+    Console.Write(num[2]);
+} else Console.Write("Третьей цифры нет");
