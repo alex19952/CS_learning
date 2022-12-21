6a33ea16c5f496824df1c61adb0857e3ff28bdd6
Alexander <alex19952@gmail.com>
2022-12-21 23:34:01 +0300

    задание 6

---
+++ 6_task_41/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+int M = 7;
+int[] arr = new int[M];
+for (int i = 0; i < 7; ++i) {
+    arr[i] = int.Parse(Console.ReadLine());
+}
+int count = 0;
+foreach (var item in arr)
+{
+    if (item > 0) count++;
+}
+Console.WriteLine(count);
---
+++ 6_task_43/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double b1 = double.Parse(Console.ReadLine());
+double k1 = double.Parse(Console.ReadLine());
+double b2 = double.Parse(Console.ReadLine());
+double k2 = double.Parse(Console.ReadLine());
+
+double opr = -k1 - -k2;
+double oprX = b1 - b2;
+double oprY = (k1*-b2) - (k2*-b1);
+
+double x = oprX / opr;
+double y = oprY / opr;
+
+Console.WriteLine($"({x}; {y})");
