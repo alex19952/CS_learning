159f195478bd0e81368e7e90e828a53b00569451
Alexander <alex19952@gmail.com>
2022-12-25 15:01:23 +0300

    задание 8

---
+++ 8_task_54/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, 
+// которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int[,] arr = {
+    {1, 4, 7, 2},
+    {5, 9, 2, 3},
+    {8, 4, 2, 4}
+};
+
+
+int rows = arr.GetUpperBound(0) + 1;
+int columns = arr.Length / rows;
+
+for (int r = 0; r < rows; r++) {
+    int[] temp = new int[columns];
+    for (int c = 0; c < columns; c++) {
+        temp[c] = arr[r,c];
+    }
+    Array.Sort(temp, (x, y) => y.CompareTo(x));
+    for (int c = 0; c < columns; c++) {
+        arr[r,c] = temp[c];
+        Console.Write($"{arr[r,c]} ");
+    }
+    Console.WriteLine();
+}
+
---
+++ 8_task_62/Program.cs
@@ -0,0 +1,91 @@
+﻿// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+string[,] arr = new string[4,4];
+
+bool right = true;
+bool down = false;
+bool left = false;
+bool up = false;
+
+int num = 1;
+
+int row = 0;
+int col = 0;
+
+int rowOut = 4;
+int colOut = 4;
+
+
+void NextStep(){
+    ++num;
+    if (right) ++col;
+    else if (down) ++row;
+    else if (left) --col;
+    else if (up) --row;
+    else Console.WriteLine("NextStep error");
+}
+
+
+void Rotate() {
+    if (right) { // поворт вниз
+        right = false;
+        down = true;
+        --col;
+    }
+    else if (down) { // поворто влево
+        down = false;
+        left = true;
+        --row;
+    }
+    else if (left) { // поворот вверх
+        left = false;
+        up = true;
+        ++col;
+        --rowOut;
+    }
+    else if (up) { // поворот вправо
+        up = false;
+        right = true;
+        ++row;
+        --colOut;
+    }
+    else Console.WriteLine("Rotate error");
+}
+
+int stopFlag = 0;
+while(true) {
+    if (col < colOut && 
+        row < rowOut &&
+        col > -1 &&
+        row > -1
+        ) {
+        if (num < 10) {
+            arr[row,col] = "0" + num.ToString();
+        }
+        else arr[row,col] = num.ToString();
+
+        NextStep();
+        stopFlag = 0;
+
+    }
+    else {
+
+        Rotate();
+        ++stopFlag;
+    }
+    if (stopFlag > 1) {
+
+        break;
+    }
+}
+
+foreach (var item in arr)
+{
+    Console.WriteLine($"{item} ");
+}
+
